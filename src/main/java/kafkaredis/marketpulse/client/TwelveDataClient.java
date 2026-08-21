7ebf6fa10a05555b6199d49529cdd42feb1bce87
Alex Eyers <alexander.eyers@gmail.com>
2026-08-21 14:27:34 +0100

    Connect stock quotes to Twelve Data API

--- src/main/java/kafkaredis/marketpulse/client/TwelveDataClient.java
+++ src/main/java/kafkaredis/marketpulse/client/TwelveDataClient.java
@@ -2,8 +2,10 @@ package kafkaredis.marketpulse.client;
 
 import kafkaredis.marketpulse.config.TwelveDataProperties;
 import kafkaredis.marketpulse.dto.StockQuoteDTO;
+import kafkaredis.marketpulse.dto.TwelveDataPriceResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -12,10 +14,24 @@ import java.time.Instant;
 @RequiredArgsConstructor
 public class TwelveDataClient implements MarketDataClient{
 
-    private final TwelveDataProperties twelveDataProperties;
+    private final TwelveDataProperties twelveDataProperties; // baseUrl and apiKey
 
     @Override
     public StockQuoteDTO getStock(String symbol) {
-        return new StockQuoteDTO(symbol, new BigDecimal("145.67"), Instant.now());
+        RestClient restClient = RestClient.create(twelveDataProperties.baseUrl());
+        TwelveDataPriceResponseDTO response = restClient.get().uri(
+                        uriBuilder -> uriBuilder
+                                .path("/price")
+                                .queryParam("symbol", symbol)
+                                .queryParam("apikey", twelveDataProperties.apiKey())
+                                .build())
+                .retrieve()
+                .body(TwelveDataPriceResponseDTO.class);
+
+        return new StockQuoteDTO(
+                symbol,
+                new BigDecimal(response.price()),
+                Instant.now()
+        );
     }
 }
