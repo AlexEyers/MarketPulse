82b8c6b18403a5bf7a48f801b7a5871d582535e9
Alex Eyers <alexander.eyers@gmail.com>
2026-08-26 01:03:18 +0100

    Fixed bug where Redis deserialization was returning JSON instead of StockQuoteDTO as expected

--- src/main/java/kafkaredis/marketpulse/config/RedisConfig.java
+++ src/main/java/kafkaredis/marketpulse/config/RedisConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import tools.jackson.databind.ObjectMapper;
 
@@ -22,10 +22,10 @@ public class RedisConfig {
 
         template.setConnectionFactory(connectionFactory); // Use this Redis connection to talk to localhost:6379 (auto-configured from .yaml)
         template.setKeySerializer(new StringRedisSerializer()); // Store Redis keys as readable Strings
-        template.setValueSerializer(new GenericJacksonJsonRedisSerializer(objectMapper)); // Store StockQuoteDTO values as JSON. (Java object -> JSON in Redis and JSON -> Java object when read back)
+        template.setValueSerializer(new JacksonJsonRedisSerializer<>(objectMapper, StockQuoteDTO.class)); // Store top-level Redis values as JSON and read them back specifically as StockQuoteDTO
 
-        template.setHashKeySerializer(new StringRedisSerializer()); // Allow us to use a Redis key's key e.g. AAPL's price key to update its value safely.
-        template.setHashValueSerializer(new GenericJacksonJsonRedisSerializer(objectMapper)); // Allow us to use a Redis key's key e.g. AAPL's price key to update its value safely.
+        template.setHashKeySerializer(new StringRedisSerializer()); // Allow us to use a Redis key's key e.g. AAPL's price key to update its value safely (otherwise opsForHash() may store/read values in a format not expected)
+        template.setHashValueSerializer(new JacksonJsonRedisSerializer<>(objectMapper, StockQuoteDTO.class)); // Store Redis hash values as JSON, and read them back specifically as StockQuoteDTO, allows us to change a Redis key's key value e.g. AAPL, price key can change 123.45 to 123.46 safely (otherwise opsForHash may store/read values in a format not expected)
 
         template.afterPropertiesSet(); // Complete RedisTemplate setup
 
